f04804db8679b1016191f0143bcf596de8d67e73
Naz <post@nazs.net>
2020-04-29 21:17:49 +0100

    first

---
+++ pages/naz.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <div class="content">
+      {{ $moment() }}
+
+    <v-parallax height="550"
+      src="https://panteli.biz/static/coffee.jpg">
+    </v-parallax>     
+    <v-parallax height="550"
+      src="https://panteli.biz/static/cup-smoke.jpg">
+    </v-parallax>     
+
+    </div>
+    <footer>
+      <p>
+        &copy; {{ $moment().year() }}
+      </p>
+    </footer>
+  </div>
+</template>
+
+<style>
+*, p {
+  margin: 0;
+  padding: 0;
+}
+html {
+  overflow: auto;
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  flex: 1 0 auto;
+}
+footer {
+  background-color: #868585;
+  height: 70px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
