f316cf408f57a06780b62ba87999787ef4126123
Naz <post@nazs.net>
2020-04-30 17:27:33 +0100

    added video background

--- plugins/core-components.js
+++ plugins/core-components.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 
 import appHeader from '@/components/header'
 import ArrowRightBoxIcon from 'vue-material-design-icons/ArrowRightBox.vue';
+import CloseCircleIcon from 'vue-material-design-icons/CloseCircle.vue';
 
 Vue.component('appHeader', appHeader)
 Vue.component('ArrowRight', ArrowRightBoxIcon)
+Vue.component('CloseCircle', CloseCircleIcon)
