354e50f7577991629a6f9f144bfe36609f5c72a7
Naz <post@nazs.net>
2020-05-01 11:06:52 +0100

    resetForm, New PIN, toggle Submit - all ok

--- pages/no-robots.vue
+++ pages/no-robots.vue
@@ -4,15 +4,15 @@
       <form class="robot">
         <div class="group name">
           <label for="name">Name:</label>
-          <input type="text" placeholder="Your name">
+          <input type="text" placeholder="Your name" v-model="visitor.name">
         </div>
         <div class="group email">
           <label for="email">Email:</label>
-          <input type="text" placeholder="Your email">
+          <input type="text" placeholder="Your email" v-model="visitor.email">
         </div>
         <div class="group message">
           <label for="message">Message</label>
-          <textarea name="" id="" cols="30" rows="10" placeholder="Your message"></textarea>
+          <textarea name="" id="" cols="30" rows="10" placeholder="Your message" v-model="visitor.message" ></textarea>
         </div>{{ pin }} {{ result }} {{ guessPin }}
         <div class="group">
           <p>Reproduce the code above using the numbers below</p>
@@ -21,8 +21,9 @@
           <i v-for="(no, index) in choice" :key="index" v-html="no.n" @click="checkPin(no.d)"></i>
         </div>
         <div class="group action">
-          <a @click="callPin">Clear</a>
-          <a @click="1">Submit</a>
+          <a @click="resetForm" class="not4Submit"><Delete title="Reset Form" fillColor="#4f0a58" :size="36" /></a>
+          <a @click="callPin" class="not4Submit"><Refresh title="New PIN" fillColor="#4f0a58" :size="36" /></a>
+          <a @click="1" class="submit" :class="{ allowForm }"><Send  :size="36" /></a>
         </div>
       </form>
   </div>
@@ -37,7 +38,7 @@ export default {
       visitor: {
         name: '', email: '', message: ''
       },
-      stopForm: true,
+      allowForm: false,
       guessPin: '',
       result: ''
     }
@@ -47,24 +48,29 @@ export default {
   },
 
   methods: {
+    resetForm() {
+      this.visitor = {}
+    },
     callPin() {
       this.$axios.$get('noRobot')
       .then(data => {
         this.pin = data
         this.guessPin = ''
-        this.result = ''  
+        this.result = ''
+        this.allowForm = false  
       })
     },
     checkPin(no) {
       let temp = ''
-      let r = this.result
       temp = temp.concat(no)
       this.guessPin = this.guessPin.concat(no)
       if(this.guessPin.length == 4) { 
-        (parseInt(this.pin) === parseInt(this.guessPin)) 
-          ? r = ' is equal to ' 
-          : r = ' is not equal to '
-        this.result = r
+        if(parseInt(this.pin) === parseInt(this.guessPin)) {
+          this.result = ' is equal to ' 
+          this.allowForm = true
+        } else {
+          this.result = ' is not equal to '
+        }
       }
     }
   }
@@ -81,7 +87,7 @@ export default {
 
 form.robot {
   background-color: rgba(255, 255, 255, 0.842);
-  color: #000;
+  // color: #000;
   width: 80%;
   display: flex;
   flex-direction: column;
@@ -106,6 +112,21 @@ form.robot {
   .group.action {
     flex-direction: row;
     justify-content: space-evenly;
+    .not4Submit {
+      cursor: pointer;
+    }
+    // .submit {
+    //   cursor: none;
+    // }
+  }
+
+  .submit {
+  color: rgb(146, 144, 144);
+  cursor: none;
+}
+  .allowForm {
+    color: #4f0a58;
+    cursor: pointer;
   }
   .choice {
     display: inline-block;
@@ -122,6 +143,7 @@ form.robot {
       font-size: 1.3em;
       color: #4f0a58;
     }
+
   }
 }
 
--- plugins/core-components.js
+++ plugins/core-components.js
@@ -4,8 +4,14 @@ import appHeader from '@/components/header'
 import appFooter from '@/components/footer'
 import ArrowRightBoxIcon from 'vue-material-design-icons/ArrowRightBox.vue';
 import CloseCircleIcon from 'vue-material-design-icons/CloseCircle.vue';
+import SendIcon from 'vue-material-design-icons/Send.vue';
+import DeleteIcon from 'vue-material-design-icons/Delete.vue';
+import RefreshIcon from 'vue-material-design-icons/Refresh.vue';
 
 Vue.component('appHeader', appHeader)
 Vue.component('appFooter', appFooter)
 Vue.component('ArrowRight', ArrowRightBoxIcon)
 Vue.component('CloseCircle', CloseCircleIcon)
+Vue.component('Send', SendIcon)
+Vue.component('Delete', DeleteIcon)
+Vue.component('Refresh', RefreshIcon)
