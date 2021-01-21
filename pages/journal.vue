e9e4a099b03819372f0df4aa843601b05a0bfc0e
Naz Sharid <post@nazs.net>
2021-01-21 21:28:05 +0000

    refactored formatting, moved to its own component

--- pages/journal.vue
+++ pages/journal.vue
@@ -18,50 +18,7 @@
     <div class="newEvent">
       <form>
         <div class="formats">
-          <div class="group1">
-            <button @click.prevent="format('richTextField', 'bold')">
-              <i class="material-icons">&#xE238;</i>
-            </button>
-            <button @click.prevent="format('richTextField', 'italic')">
-              <i class="material-icons">&#xE23F;</i>
-            </button>
-            <button @click.prevent="format('richTextField', 'underline')">
-              <i class="material-icons">&#xE249;</i>
-            </button>
-            <button @click.prevent="format('richTextField', 'insertunorderedlist')">
-              <i class="material-icons">&#xE241;</i>
-            </button>
-            <button @click.prevent="format('richTextField', 'insertorderedlist')">
-              <i class="material-icons">&#xE242;</i>
-            </button>
-          </div>
-          <div class="group2">
-            <div class="selects">
-              <select
-                ref="FontName"
-                @change="formatMore('FontName', 'richTextField', 'FontName')"
-              >
-                <option value="0">Change Font</option>
-                <template v-for="(font, index) in fontNames">
-                  <option :value="font.name" class="fonts" :key="index" :class="font.key">
-                    {{ font.name }}
-                  </option>
-                </template>
-              </select>
-
-              <select
-                ref="FontSize"
-                @change="formatMore('FontSize', 'richTextField', 'FontSize')"
-              >
-                <option value="0">Font Size</option>
-                <template v-for="(size, index) in fontUnit">
-                  <option :value="size.name" class="fonts" :key="index" :class="size.key">
-                    {{ size.name }}
-                  </option>
-                </template>
-              </select>
-            </div>
-          </div>
+          <formattings />
         </div>
         <textarea
           style="display: none; font-family: Courier"
@@ -95,8 +52,6 @@ export default {
     return {
       tips: [],
       search: '',
-      fontNames: wys.fontNames,
-      fontUnit: wys.fontUnit,
       el: '',
     }
   },
@@ -139,18 +94,6 @@ export default {
     },
     clear() {
       window.frames['richTextField'].document.body.innerHTML = ''
-    },
-    format(el, att) {
-      wys.formatBasic(el, att)
-    },
-    formatMore(ref, el, att) {
-      // let item = opt == 'font' ? 'FontName' : 'FontSize'
-      var ele = this.$refs[ref]
-      console.log(ele)
-      var myFont = ele.options[ele.selectedIndex].value
-      console.log(myFont)
-      wys.formatMore(el, att, myFont)
-      ele.value = 0
     },
     submitLog() {
       // this.log.comment = wys.showForm(this.el)
